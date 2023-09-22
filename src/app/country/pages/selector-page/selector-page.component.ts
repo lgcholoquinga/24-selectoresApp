41d1ebb4d16d077df43c5a796357b201fa36e46f
lgcholoquinga <luisg.choloquingal@gamil.com>
2023-09-21 20:04:30 -0500

    iframe

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, NgZone, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interface';
 import { filter, switchMap, tap } from 'rxjs';
+
+interface User {
+  userName: string;
+  password: string;
+  origin?: string;
+}
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
   styleUrls: ['./selector-page.component.scss'],
 })
-export class SelectorPageComponent implements OnInit {
+export class SelectorPageComponent implements OnInit, AfterViewInit {
   public countries: SmallCountry[] = [];
   public borders: SmallCountry[] = [];
+  public user!: User;
 
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
@@ -21,8 +28,28 @@ export class SelectorPageComponent implements OnInit {
 
   constructor(
     private readonly fb: FormBuilder,
-    private readonly srvCountry: CountriesService
-  ) {}
+    private readonly srvCountry: CountriesService,
+    private ngZone: NgZone
+  ) {
+    window.addEventListener(
+      'message',
+      (e: MessageEvent) => {
+        if (e.data != undefined) {
+          const { userName, password } = e.data;
+          this.user = {
+            userName,
+            password,
+            origin: e.origin,
+          };
+        }
+      },
+      false
+    );
+  }
+
+  ngAfterViewInit(): void {
+    window.parent.postMessage({ ok: true }, '*');
+  }
 
   ngOnInit(): void {
     this.onRegionChange();
